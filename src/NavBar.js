5467f88b94e43f5885bdba33b23e1da9fb9b5c65
Nahuel Medina <nahuelmedina1230@hotmail.com>
2023-07-26 10:06:02 -0300

    Clase nro 2

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+
+function NavBar () {
+    return (
+        <div className="NavBar">
+            <nav>
+                <a href="#">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+            </nav>
+        </div>
+    )
+}
+
+
+export default NavBar 
